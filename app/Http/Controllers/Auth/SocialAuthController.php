a9f11fa7cf56d58e18727264379d19236f4f69ad
Shaikh Safiya Naaz Abdul Hakeem <safiya.f30@gmail.com>
2019-02-26 13:17:22 +0530

    github login exception
    Not yet completed

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Foundation\Auth;
+use App\SocialIdentity;
+//use Illuminate\Foundation\Auth;
 use App\User;
+use Auth;
+use Redirect;
 
 class SocialAuthController extends Controller
 {
@@ -16,57 +19,80 @@ class SocialAuthController extends Controller
     	return Socialite::driver('github')->redirect();
     }
 
-    public function handleProviderCallback()
-    {
-//     	 $user = Socialite::driver('github')->user();
-//     	 $token = $user->token;
-// 		$refreshToken = $user->refreshToken; // not always provided
-// 		$expiresIn = $user->expiresIn;
+    public function handleProviderCallback ()
+   {
+       try {
+           $user = Socialite::driver('github')->user();
+       } 
+       catch (Exception $e) 
+       {
+           return redirect('/login');
+       }
 
-// // OAuth One Providers
-// 		$token = $user->token;
-// 		$tokenSecret = $user->tokenSecret;
-
-// 		// All Providers
-// 		$user->getId();
-// 		$user->getNickname();
-// 		$user->getName();
-// 		$user->getEmail();
-// 		$user->getAvatar();
-    	try {
-            $user = Socialite::driver('github')->user();
-
-        } catch (\Exception $e) {
-            return redirect('/login');
-        }
-        // only allow people with @company.com to login
-        // if(explode("@", $user->email)[1] !== 'company.com'){
-        //     return redirect()->to('/');
-        // }
-        // check if they're an existing user
-        $existingUser = User::where('email', $user->email)->first();
-        if($existingUser){
-            // log them in
-            auth()->login($existingUser, true);
-
-        } else {
-            // create a new user
-            $newUser                  = new User;
-            $newUser->name            = $user->name;
-            $newUser->email           = $user->email;
-            $newUser->google_id       = $user->id;
-            $newUser->avatar          = $user->avatar;
-            $newUser->avatar_original = $user->avatar_original;
-            $newUser->save();
-            auth()->login($newUser, true);
-        }
-        return redirect('/home');
-    }
-
-    	//get the user
+       $authUser = $this->findOrCreateUser($user);
+       Auth::login($authUser, true);
+       return Redirect::to($this->redirectTo);
+   }
 
 
-    	//store user info
+   public function findOrCreateUser($providerUser)
+   {
+       $account = SocialIdentity::whereProviderName('github')
+                  ->whereProviderId($providerUser->getId())
+                  ->first();
+
+       if ($account) {
+           return $account->user;
+       } else {
+           $user = User::whereEmail($providerUser->getEmail())->first();
+
+           if (! $user) {
+               $user = User::create([
+                   'email' => $providerUser->getEmail(),
+                   'name'  => $providerUser->getName(),
+               ]);
+           }
+
+           $user->identities()->create([
+               'provider_id'   => $providerUser->getId(),
+               'provider_name' => 'github',
+           ]);
+
+           return $user;
+       }
+   }
+
+}
+   
+
+
+    // public function handleProviderCallback()
+    // {
+    // 	try {
+    //         $user = Socialite::driver('github')->user();
+
+    //     } catch (\Exception $e) {
+    //         return redirect('/login');
+    //     }
+    //     $existingUser = User::where('email', $user->email)->first();
+    //     if($existingUser){
+    //         // log them in
+    //         auth()->login($existingUser, true);
+
+    //     } else {
+    //         // create a new user
+    //         $newUser                  = new User;
+    //         $newUser->name            = $user->name;
+    //         $newUser->email           = $user->email;
+    //         $newUser->google_id       = $user->id;
+    //         $newUser->avatar          = $user->avatar;
+    //         $newUser->avatar_original = $user->avatar_original;
+    //         $newUser->save();
+    //         auth()->login($newUser, true);
+    //     }
+    //     return redirect('/home');
+    // }
+
     	
     
-}
+
