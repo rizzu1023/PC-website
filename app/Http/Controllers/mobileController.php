5f68ea75619232ff030fb0caa40553a321472c5a
Azamn <azamali.shaikh@gmail.com>
2019-03-01 20:49:16 +0530

    Done almost backened

--- app/Http/Controllers/mobileController.php
+++ app/Http/Controllers/mobileController.php
@@ -12,7 +12,12 @@ class mobileController extends Controller
     //
 
     public function Insert(Request $request,Response $response){    
-        event_particpants::create($request->all);
+        $insert = event_particpants::create($request->all);
         return response()->json($eventparticipant,200);
+    }
+
+    public function fetch(Request $request,Response $response){
+        $store = event_particpants::all();
+        return response()->json($store,200); 
     }
 }
