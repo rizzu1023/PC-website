8effb92f0c5095a0787e8b3fce3920c3e4189522
Mohammed Rizwan <mrizwan9022@gmail.com>
2019-03-02 10:58:24 +0530

    update found

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,20 +11,46 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     // public function __construct()
     // {
     //     $this->middleware(['auth','admin']);
     // }
 
 
-    public function index()
+    public function Home()
     {
-        return view('/home');
+        return view('pc.index');
     }
+
+    public function getEvent()
+    {
+        return view('pc.event');
+    }
+
+    public function getAbout()
+    {
+        return view('pc.about');
+    }
+
+    public function getRegister()
+    {
+        return view('pc.register');
+    }
+
+    public function getContact()
+    {
+        return view('pc.contact');
+    }
+
+    public function getBlog(){
+        return view('pc.blog');
+    }
+
+
 
 
 }
